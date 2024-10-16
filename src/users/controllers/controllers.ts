872ba0045e4d95042d096bf04ef0f119015d249c
Juan Gonon <juanfegonon27@gmail.com>
2024-10-16 11:35:35 -0600

    getUserById and deleteUser

--- src/users/controllers/controllers.ts
+++ src/users/controllers/controllers.ts
@@ -5,27 +5,6 @@ import { CreateUserDTO } from '../../domain/DTOs/users/createUser.dto'
 import { RespUserDTO } from '../../domain/DTOs/users/respUser.dto'
 import { UpdateUserDTO } from '../../domain/DTOs/users/updateUser.dto'
 
-const users = [
-  {
-    id: 1,
-    name: 'Kamila',
-    lastname: 'Valieva',
-    createdAt: new Date('2024-10-13')
-  },
-  {
-    id: 2,
-    name: 'Anna Shcherbakova',
-    lastname: 'Sch',
-    createdAt: new Date('2024-10-13')
-  },
-  {
-    id: 3,
-    name: 'Kamila',
-    lastname: 'Valieva',
-    createdAt: null
-  }
-]
-
 export class UsersController {
   getUsers = async (req: Request, res: Response): Promise<void> => {
     const users = await prisma.users.findMany()
@@ -37,17 +16,19 @@ export class UsersController {
   getUserById = async (req: Request, res: Response): Promise<void> => {
     const userId = +req.params.id
 
-    const userById = users.find((user) => user.id === userId)
+    const user = await prisma.users.findFirst({
+      where: {
+        id: userId
+      }
+    })
 
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (!userById) {
-      res.status(404).json({
-        error: 'Usuario no encontrado'
-      })
+    if (!user) {
+      res.status(404).json({ error: `Todo with id ${userId} not found` })
       return
     }
+    const userResp = RespUserDTO.response(user)
 
-    res.json(userById)
+    res.json(userResp)
   }
 
   createUser = async (req: Request, res: Response): Promise<void> => {
@@ -90,13 +71,17 @@ export class UsersController {
   deleteUser = async (req: Request, res: Response): Promise<void> => {
     const userId = +req.params.id
 
-    const todoIndex = users.findIndex((user) => user.id === userId)
-    const todo = users[todoIndex]
-    if (todoIndex < 0) {
-      res.status(404).json({ error: `Todo with id ${userId} not found` })
+    const user = await prisma.users.findFirst({ where: { id: userId } })
+
+    if (!user) {
+      res.status(404).json(
+        { error: `Todo with id ${userId} not found` }
+      )
       return
     }
-    users.splice(todoIndex, 1)
-    res.json(todo)
+
+    const deletedUser = await prisma.users.delete({ where: { id: userId } })
+    const userDeleteResp = RespUserDTO.response(deletedUser)
+    res.json(userDeleteResp)
   }
 }
