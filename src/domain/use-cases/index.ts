5f0e0b43a2ff685e1e5b54ea963aab6c2a116d31
Juan Gonon <juanfegonon27@gmail.com>
2024-10-17 11:50:18 -0600

    update user useCase

--- src/domain/use-cases/index.ts
+++ src/domain/use-cases/index.ts
@@ -1,3 +1,4 @@
 export * from './user/getsUsers'
 export * from './user/getByIdUser'
 export * from './user/create-User'
+export * from './user/update-user'
--- src/users/controllers/controllers.ts
+++ src/users/controllers/controllers.ts
@@ -1,9 +1,8 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { Request, Response } from 'express'
 import { RespUserDTO } from '../../domain/DTOs/users/respUser.dto'
-import { UpdateUserDTO } from '../../domain/DTOs/users/updateUser.dto'
 import { UserRepository } from '../../domain/repository/user.repository'
-import { GetsUsers, GetByIdUser, CreateUser } from '../../domain/use-cases'
+import { GetsUsers, GetByIdUser, CreateUser, UpdateUser } from '../../domain/use-cases'
 
 export class UsersController {
   constructor (private readonly repository: UserRepository) {}
@@ -31,17 +30,12 @@ export class UsersController {
 
   updateUser = async (req: Request, res: Response): Promise<void> => {
     const userId = +req.params.id
-    try {
-      const updateUser = UpdateUserDTO.update({ ...req.body, id: userId })
-      const user = await this.repository.update(updateUser)
-      const resUserUpdated = RespUserDTO.response(user)
 
-      res.json(resUserUpdated)
-    } catch (error) {
-      res.status(400).json({
-        error: (error as Error).message
-      })
-    }
+    const updateUser = new UpdateUser(this.repository)
+
+    updateUser.execute({ ...req.body, id: userId })
+      .then((user) => res.json(user))
+      .catch((error) => res.status(400).json({ ok: false, error: (error as Error).message }))
   }
 
   deleteUser = async (req: Request, res: Response): Promise<void> => {
